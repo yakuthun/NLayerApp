ab759854ac54f9afca1b3a9e0f520b38a4ec47e5
yakuthun <yakuthun@gmail.com>
2022-08-04 10:26:23 +0300

    Core Layer finish

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        //productRepository.GetAll(x=> x.id>5).toList();
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+
+        //productRepository.where(x=>x.id>5).ToListAsync;
+        IQueryable<T> Where(Expression<Func<T,bool>> expression);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task<T> AddAsync(T entity); //create, insert
+        Task AddRangeAsync (IEnumerable<T> entities);
+       void Update(T entity);
+       void Remove(T entity);
+       void RemoveRange(IEnumerable<T> entities);
+    }
+}
---
+++ NLayer.Core/Services/IService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        //productRepository.GetAll(x=> x.id>5).toList();
+        Task<IEnumerable<T>> GetAllAsync();
+
+        //productRepository.where(x=>x.id>5).ToListAsync;
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task<T> AddAsync(T entity); //create, insert
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+    }
+}
